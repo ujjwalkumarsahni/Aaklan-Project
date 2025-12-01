ca9a6d12ee9e6b33256763a727a7d42f66bc22db
ujjwalkumarsahni <ujjwalkumar0514@gmail.com>
2025-12-01 13:51:26 +0530

    fix image

--- frontend/src/assets/OurBelivers/OurBelivers.js
+++ frontend/src/assets/OurBelivers/OurBelivers.js
@@ -4,7 +4,7 @@ import skyworld from '../OurBelivers/skyworld.jpg'
 import sas from '../OurBelivers/sas.png'
 import chatrpati from '../OurBelivers/chhatrpati.gif'
 import mac from '../OurBelivers/mac.png'
-import mhs from '../OurBelivers/mhs.jpg'
+import mhs from '../OurBelivers/MHS.jpg'
 
 export const ourBelivers = {
     gps,
